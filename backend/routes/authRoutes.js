6ea52ddf663bccc930505285089bba2a10e7ef65
quietjun802 <whdydwns802@gmail.com>
2025-08-22 10:17:56 +0900

    1

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,12 +5,28 @@ const {v4:uuidv4}=require('uuid')
 
 router.post('/guest',(req,res)=>{
     try {
-
+        
         if(!process.env.JWT_SECRET){
             return res.status(500).json({message:"서버 설정 오류 : JWT_SECRET이 없습니다."})
         }
 
         const uid =(req.body && req.body.deviceId)? String(req.body.deviceId):uuidv4()
+
+        const token =jwt.sign(
+            {uid, role:'guest'},
+            process.env.JWT_SECRET,
+            {expiresIn:'7d'}
+        )
+
+        res.cookie('auth',token,{
+            httpOnly:true,
+            sameSite:'none',
+            secure:false,
+            maxAge:7*24*60*60*1000,
+            path:'/'
+        })
+
+        return res.status(200).json({message:'게스트 인증 완료',uid})
 
     } catch (error) {
         console.error("게스트 인증 오류",error)
